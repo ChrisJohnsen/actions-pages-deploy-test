fc6c0442faf71bf33b559735a386d2494c4c8546
Chris Johnsen <chris_johnsen@pobox.com>
2023-11-21 00:57:21 -0600

    fix dump tar mistake; make link with base value

--- scripts/build.js
+++ scripts/build.js
@@ -11,6 +11,15 @@ const dir =
     },
     void 0
   ) ?? 'dist/'
+const base =
+  args.reduce(
+    (r, a) => {
+      const opt = '--base='
+      if (a.startsWith(opt)) return a.slice(opt.length)
+      return r
+    },
+    void 0
+  ) ?? '/'
 
 const file = join(dir, 'index.html')
 
@@ -18,7 +27,10 @@ await mkdir(dir, { recursive: true })
 console.log(`created dirs for ${dir}`)
 await writeFile(
   file,
-  `<html><body><p>Built into <code>${esc(dir)}</code> at ${esc(new Date())}</p></body></html>`
+  `<html><body>
+    <p>Built into <code>${esc(dir)}</code> at ${esc(new Date())}</p>
+    <p><a href="${encodeURI(base + '/index.html')}">This</a> is me?</p>
+  </body></html>`
 )
 console.log(`wrote ${file}`)
 
