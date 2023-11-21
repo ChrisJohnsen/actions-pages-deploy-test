4cc2463fc8a9c6578731988f8a14e16ce898f582
Chris Johnsen <chris_johnsen@pobox.com>
2023-11-20 23:54:45 -0600

    first try

---
+++ scripts/build.js
@@ -0,0 +1,30 @@
+import { mkdir, writeFile } from 'node:fs/promises'
+import { join } from 'node:path'
+
+const args = process.argv.slice(2)
+const dir =
+  args.reduce(
+    (r, a) => {
+      const opt = '--outDir='
+      if (a.startsWith(opt)) return a.slice(opt.length)
+      return r
+    },
+    void 0
+  ) ?? 'dist/'
+
+const file = join(dir, 'index.html')
+
+await mkdir(dir, { recursive: true })
+await writeFile(
+  file,
+  `<html><body><p>Built into <code>${esc(dir)}</code> at ${esc(new Date())}</p></body></html>`
+)
+
+/**
+ *
+ * @param {String} s
+ */
+function esc(s) {
+  if (typeof s != 'string') s = String(s)
+  return s.replace(/[<>&'"]/g, (c) => `&#${c.codePointAt(0)};`)
+}
